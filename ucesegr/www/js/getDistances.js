7beba76c9e221fe7b61c007fec9189885137fd9d
EGwebbing <ucesegr@ucl.ac.uk>
2018-05-15 04:38:48 -0400

    getdistance

--- ucesegr/www/js/getDistances.js
+++ ucesegr/www/js/getDistances.js
@@ -124,7 +124,7 @@ function dataUploadedAnswer() {
 	if (client.readyState == 4) {
 		// make an alert to show the response
 		alert("Data uploaded correctly") +
-		getDistance();
+		setTimeout(getDistance,20000);
 		//document.getElementById("dataUploadResult").innerHTML = "Data uploaded correctly";
 	}
 }	
